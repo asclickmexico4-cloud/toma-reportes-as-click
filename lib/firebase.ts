b97f9ab37158ee467afec984cc0bad31fdf729e1
asclickmexico4-cloud <asclickmexico4@gmail.com>
2026-03-10 03:51:43 -0600

    fix conflictos

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -8,27 +7,9 @@ const firebaseConfig = {
   projectId: "asclick-6faf9",
   storageBucket: "asclick-6faf9.firebasestorage.app",
   messagingSenderId: "501315978480",
-  appId: "1:501315978480:web:58aa1bfab5e71bbc66ae35"
+  appId: "1:501315978480:web:58aa1bfab5e71bbc66ae35",
 };
-
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
-=======
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAcYsNqiD3mdowWngvqZhXIyGSIvqpzny8",
-  authDomain: "asclick-6faf9.firebaseapp.com",
-  projectId: "asclick-6faf9",
-  storageBucket: "asclick-6faf9.firebasestorage.app",
-  messagingSenderId: "501315978480",
-  appId: "1:501315978480:web:58aa1bfab5e71bbc66ae35"
-};
-
-
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
->>>>>>> 83fcb5c5b5a521ad311fed5da20f218b4a2fed05
 export const db = getFirestore(app);
